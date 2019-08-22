6a7e9cc834a0be320020cfc099a54374476e30d6
PatSos-intern7 <intern7@wearevirtua.com>
2019-08-22 15:25:54 +0200

    added cruds
    Added cruds to Book, Author, BookGenre

---
+++ src/Form/BookGenreType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\BookGenre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookGenreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('edit_count')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => BookGenre::class,
+        ]);
+    }
+}
