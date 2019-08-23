5146f4d592ba1c9fae32e0f30c5646a62ead3e19
PatSos-intern7 <intern7@wearevirtua.com>
2019-08-23 09:41:48 +0200

    update Author fied type.

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,7 @@ class AuthorType extends AbstractType
         $builder
             ->add('name')
             ->add('surname')
-            ->add('countryOfOrigin')
+            ->add('countryOfOrigin', CountryType::class)
             ->add('yearOfBirth')
             ->add('yearOfDead')
         ;
