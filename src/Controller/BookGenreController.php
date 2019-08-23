edfde13311db14126d96d4c32526b50b66bb1815
PatSos-intern7 <intern7@wearevirtua.com>
2019-08-23 08:29:50 +0200

    added /library

--- src/Controller/BookGenreController.php
+++ src/Controller/BookGenreController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/book/genre")
+ * @Route("/library/genre")
  */
 class BookGenreController extends AbstractController
 {
