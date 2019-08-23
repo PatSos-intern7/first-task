89282ae492b7581fbc2798fd2606b9d463ab1ac4
PatSos-intern7 <intern7@wearevirtua.com>
2019-08-23 12:54:12 +0200

    update bookgenre form field and view

--- src/Form/BookGenreType.php
+++ src/Form/BookGenreType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\BookGenre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class BookGenreType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('description')
+            ->add('description',TextareaType::class)
             ->add('edit_count')
         ;
     }
