4b5319df42767a09850717c17cedece27e8ba534
PatSos-intern7 <intern7@wearevirtua.com>
2019-08-23 11:56:52 +0200

    booktype form update.

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\BookGenre;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +18,19 @@ class BookType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('description')
-            ->add('author')
+            ->add('description',TextareaType::class)
+            ->add('author',EntityType::class,[
+                'class'=>Author::class,
+                'choice_label'=> function($author){
+                    return $author->getName()." ".$author->getSurname();
+                }
+            ])
+            ->add('genre',EntityType::class,[
+                'class'=>BookGenre::class,
+                'choice_label'=>'name'
+            ])
             ->add('Publish')
-            ->add('country')
+            ->add('country',CountryType::class)
             ->add('availability')
         ;
     }
