1126727eb0540872073df14d9d6fbcb7e85e001b
PatSos-intern7 <intern7@wearevirtua.com>
2019-09-03 09:44:26 +0200

    step1 of instalation done.
    composer require annotations
    composer require swiftmailer-bundle
    composer require twig
    composer require translations
    composer require server --dev

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
