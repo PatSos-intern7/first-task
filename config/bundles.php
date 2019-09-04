5acb6b5706b332d2e3e408781062bc48c7e2b6ea
PatSos-intern7 <intern7@wearevirtua.com>
2019-09-04 10:10:31 +0200

    Add FOS REST bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
