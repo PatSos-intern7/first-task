b24cb3bb8fc521c46ad1c0e100ec59c917a9fb19
PatSos-intern7 <intern7@wearevirtua.com>
2019-09-06 12:06:25 +0200

    add export Manager

--- src/Service/ExporterManager.php
+++ src/Service/ExporterManager.php
@@ -2,64 +2,37 @@
 
 namespace App\Service;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\Repository\RepositoryFactory;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ExporterManager
 {
-    protected $repository;
-    protected $entityName;
-    protected $em;
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(ProductRepository $productRepository, SerializerInterface $serializer)
     {
-        $this->em = $entityManager;
+        $this->productRepository = $productRepository;
+        $this->serializer = $serializer;
     }
 
-    public function pickSelected(string $class, array $idList=[])
+    public function createCsv($filename,$ids)
     {
-       return $this->em->getRepository('App\Entity\\'.$class)->createQueryBuilder('q')
-            ->select('q.id','q.name')->andWhere('q.id IN(:ids)')
-            ->setParameter('ids',$idList)
-            ->getQuery()
-            ->getResult();
-    }
-
-    public function getData(string $class, array $idList=[])
-    {
-        $data = $this->em->getRepository('App\Entity\\'.$class);
-        if(!empty($idList[0])){
-            return $this->pickSelected($class, $idList);
+        if ($ids) {
+            $data = $this->serializer->serialize($this->productRepository->findBy(['id' => $ids]), 'csv');
+        } else {
+            $data = $this->serializer->serialize($this->productRepository->findAll(), 'csv');
         }
-
-        return $data->createQueryBuilder('q')
-                    ->select('q.id,q.name')
-                    ->getQuery()
-                    ->getResult();
-    }
-
-    public function createCsv(string $filename, $class, $ids)
-    {
-
-        $ids = explode(",",$ids);
-        $data = $this->getData($class, $ids);
-        $result = [];
-
-        foreach ($data as $key => $item) {
-            $result[$key]['id'] = $item['id'];
-            $result[$key]['name'] = $item['name'];
-        }
-
         $file = fopen($filename.'.csv','w');
-        fputcsv($file, ['id','name']);
-        foreach ($result as $line) {
-            fputcsv($file,$line);
-        }
+        fwrite($file,$data);
         fclose($file);
     }
+
 }
