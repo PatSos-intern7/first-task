dd923c584b61644138e2a1b3d5fbcc526042657d
PatSos-intern7 <intern7@wearevirtua.com>
2019-09-11 14:50:25 +0200

    add ondelete cascade to image and migration

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -34,6 +34,7 @@ class Image
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Product", inversedBy="imageGallery")
+     * @ORM\JoinColumn(name ="product_id", referencedColumnName="id", onDelete = "CASCADE", nullable=false)
      */
     private $product;
 
