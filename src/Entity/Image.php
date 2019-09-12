a9923f3b801014f1034b96ebacee8f82bf89e968
PatSos-intern7 <intern7@wearevirtua.com>
2019-09-12 10:01:39 +0200

    add delete image gallery option from show product view

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
  */
-class Image
+class Image extends \Symfony\Component\Validator\Constraints\Image
 {
     /**
      * @ORM\Id()
